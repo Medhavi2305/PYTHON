d4398fa3cf253c5ee1300d9ee5d088e3c8c61b5b
Medhavi <medhavisharma2305@gmail.com>
2026-07-26 23:05:41 +0000

    Tuple in Python

---
+++ 06_Tuple.py
@@ -0,0 +1,6 @@
+tea_types = ("Oolong", "White", "empty", "Black", "Green", "Da Hong Pao", "Panda Dung Tea", "Vintage Pu'erh Tea", "Gyokuro", "Yellow Gold Buds", "Herbal")
+
+if "Green" in tea_types:
+   print("Yes i have")
+
+print(tea_types.count("Oolong"))
