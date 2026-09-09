2ab5a8fa36f3df485bbdc7e0f70c8ffcdfc011e8
Medhavi <medhavisharma2305@gmail.com>
2026-09-09 22:42:24 +0000

    Tuple in Python

--- 06_Tuple.py
+++ 06_Tuple.py
@@ -4,3 +4,8 @@ if "Green" in tea_types:
    print("Yes i have")
 
 print(tea_types.count("Oolong"))
+
+
+more_teas = ("Black", "Green", "Oolong")
+(black, green, oolong) = more_teas # By this we can access more_teas element by the name
+print(more_teas)
